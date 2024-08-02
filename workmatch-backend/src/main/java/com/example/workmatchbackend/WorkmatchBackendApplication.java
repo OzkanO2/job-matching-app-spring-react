f44ac52a7ac5e8eaf2a1407af53bcc7797f23d00
halil ozkara <ozkan210900@gmail.com>
2024-08-02 02:41:00 +0200

    error 403 to do from this point

--- workmatch-backend/src/main/java/com/example/workmatchbackend/WorkmatchBackendApplication.java
+++ workmatch-backend/src/main/java/com/example/workmatchbackend/WorkmatchBackendApplication.java
@@ -2,8 +2,9 @@ package com.example.workmatchbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WorkmatchBackendApplication {
 
 	public static void main(String[] args) {
