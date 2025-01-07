68ced52fc4f5a12bfa5a3c5a84b7cf46e50c9446
unknown <ozkan210900@gmail.com>
2025-01-07 08:22:09 +0100

    working on dependencies

--- workmatch-frontend/metro.config.cjs
+++ workmatch-frontend/metro.config.cjs
@@ -2,4 +2,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts.push('mjs'); // Ajoute le support des fichiers .mjs
+
 module.exports = config;
